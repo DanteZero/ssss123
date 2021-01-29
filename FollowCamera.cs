4992322c7b49c05278bb2037be9109ab107992da
DanteZero <37395826+DanteZero@users.noreply.github.com>
2021-01-29 08:45:32 +0000

    Add files via upload

---
+++ FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.SetParent(this.camera.transform);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
